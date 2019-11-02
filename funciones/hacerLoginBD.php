58f276f4e54bc3a8a668671dc4d2032e7cab1f05
octaviovillegas <el.octavio.villegas@gmail.com>
2019-11-02 12:06:19 -0300

    hacerlogin db 2

--- funciones/hacerLoginBD.php
+++ funciones/hacerLoginBD.php
@@ -1,9 +1,28 @@
-<h1>bienvenido</h1>
+
 
 <?php
-	
-echo "bienvenido";
+	include 'AccesoDatos.php';
+
 	$usuarioIngresado = $_GET['inputEmail'];
 	$claveIngresada = $_GET['inputPassword'];
-	
+
+	include 'AccesoDatos.php';
+	$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+	$consulta =$objetoAccesoDato->RetornarConsulta("select * from usuario");
+	$consulta->execute();			
+	$datos= $consulta->fetchAll(PDO::FETCH_ASSOC);		
+	foreach ($datos as $usuario ) {
+		if($usuario["nombre"]==$usuarioIngresado)
+		{
+			if($usuario["clave"]==$claveIngresada)
+			{
+
+				$_SESSION['usuario']=$objeto->usuario;
+				$_SESSION['perfil']=$objeto->perfil;
+				setcookie("usuario", $_SESSION['usuario']);
+			}
+		}
+		# code...
+	}
+
 ?>
